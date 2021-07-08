e9e9ad0a55643cd515ed906d448cb73f6dab4835
mide7 <imharyomide@gmail.com>
2021-07-08 17:23:33 +0100

    changes to testimonial section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import Quotes from "../img/inverted-commas.svg";
 
 function Testimonials() {
   return (
     <main className="testimonial-bg">
-      <div className="container mx-auto flex flex-col">
+      <div className="container mx-auto flex flex-col relative">
         <div className="mt-8 flex flex-col gap-8 text-center items-center font-semibold">
           <h2 className="capitalize text-blue-primary text-3xl">
             testimonials
           </h2>
-          <figure className="bg-white max-w-3/4 flex flex-col gap-8 pt-12 px-12 mb-12 shadow-md">
+
+          <img src={Quotes} alt="" className="absolute transform rotate-180 lg:left-20 lg:top-16  lg:w-16  w-12" />
+          <figure className="bg-white max-w-3/4 flex flex-col gap-8 pt-12 px-12 mb-12 shadow-lg">
             <div>
               It is a long established fact that a reader will be distracted by
               the readable content of a page when looking at its layout. The
@@ -18,15 +21,33 @@ function Testimonials() {
               distribution of letters, as opposed to using 'Content here,
               content here', making it look like readable English. Many desktop
             </div>
-            <figcaption className="text-blue-primary">----- Micheal Steve</figcaption>
-            <div className='mb-4'>
-              <FontAwesomeIcon icon={faCircle} className='text-xs text-gray-300 mx-2'/>
-              <FontAwesomeIcon icon={faCircle} className='text-xs text-gray-300 mx-2'/>
-              <FontAwesomeIcon icon={faCircle} className='text-xs text-blue-primary mx-2'/>
-              <FontAwesomeIcon icon={faCircle} className='text-xs text-gray-300 mx-2'/>
-              <FontAwesomeIcon icon={faCircle} className='text-xs text-gray-300 mx-2'/>
+            <figcaption className="text-blue-primary">
+              ----- Micheal Steve
+            </figcaption>
+            <div className="mb-4">
+              <FontAwesomeIcon
+                icon={faCircle}
+                className="text-xs text-gray-300 mx-2"
+              />
+              <FontAwesomeIcon
+                icon={faCircle}
+                className="text-xs text-gray-300 mx-2"
+              />
+              <FontAwesomeIcon
+                icon={faCircle}
+                className="text-xs text-blue-primary mx-2"
+              />
+              <FontAwesomeIcon
+                icon={faCircle}
+                className="text-xs text-gray-300 mx-2"
+              />
+              <FontAwesomeIcon
+                icon={faCircle}
+                className="text-xs text-gray-300 mx-2"
+              />
             </div>
           </figure>
+          <img src={Quotes} alt="" className="absolute right-20 bottom-4 w-16" />
         </div>
       </div>
     </main>
